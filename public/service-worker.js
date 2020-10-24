c373b4bb6c4a8d1f51e66ea7b009d135dfc804c9
Geovany Cardoza <geoyd17@gmail.com>
2020-10-24 14:27:34 -0400

    fixing manifest and service worker files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,19 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "/db.js", "/style.css"];
+const FILES_TO_CACHE = [
+  "/",
+  "/styles.css",
+  "/index.js",
+  "/index.html",
+  "/images/icon-192x192.png",
+  "/images/icon-512x512.png",
+];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Your files were pre-cached successfully!");
+      console.log("Cached");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -15,14 +21,13 @@ self.addEventListener("install", function (evt) {
   self.skipWaiting();
 });
 
-// activate
 self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
+            console.log("Removed", key);
             return caches.delete(key);
           }
         })
@@ -33,32 +38,38 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-// fetch
-self.addEventListener("fetch", (evt) => {
+self.addEventListener("fetch", function (evt) {
   if (evt.request.url.includes("/api/")) {
-    console.log("[Service Worker] Fetch(data)", evt.request.url);
-
     evt.respondWith(
-      caches.open(DATA_CACHE_NAME).then((cache) => {
-        return fetch(evt.request)
-          .then((response) => {
-            if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch((err) => {
-            return cache.match(evt.request);
-          });
-      })
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
     );
+
     return;
   }
 
   evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
       });
     })
   );
