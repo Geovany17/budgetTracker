b431517344e37024b0596c15350de22451fb9e6a
Geovany Cardoza <geoyd17@gmail.com>
2020-10-24 23:14:15 -0400

    trying to deploy again

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,20 @@
 const FILES_TO_CACHE = [
   "/",
-  "index.html",
-  "db.js",
+  "styles.css",
   "index.js",
-  "style.css",
-  "favicon.ico",
+  "db.js",
+  "index.html",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log("Your files were pre-cached successfully!");
+      console.log("Cached");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -22,14 +22,13 @@ self.addEventListener("install", function (evt) {
   self.skipWaiting();
 });
 
-// activate
 self.addEventListener("activate", function (evt) {
   evt.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log("Removing old cache data", key);
+            console.log("Removed", key);
             return caches.delete(key);
           }
         })
@@ -40,12 +39,38 @@ self.addEventListener("activate", function (evt) {
   self.clients.claim();
 });
 
-// fetch
 self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+
+    return;
+  }
+
   evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/");
+        }
       });
     })
   );
